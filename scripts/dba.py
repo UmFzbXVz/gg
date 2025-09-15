4c8d9b5873d28b4a478da24744c0ce76256e6acb
UmFzbXVz <63111902+UmFzbXVz@users.noreply.github.com>
2025-09-15 12:15:44 +0200

    Update dba.py

--- scripts/dba.py
+++ scripts/dba.py
@@ -1,6 +1,6 @@
 const MAX_RESULTS = 600;
 const PROXY = "https://corsproxy.io/?";
-const PRICE_FILE = "/gg/docs/priser.json.gz";
+const PRICE_FILE = PROXY + encodeURIComponent("https://github.com/UmFzbXVz/gg/raw/refs/heads/main/docs/priser.json.gz");
 
 (async () => {
 	const grid = document.getElementById("grid");
